95f92b62d9e3978d716636a39e628f9b73145188
Laela Putri Salsa Biella <112465346+salsabiellalp@users.noreply.github.com>
2022-09-01 15:57:31 +0700

    Update settings.py

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
